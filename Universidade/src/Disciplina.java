0e0ab3743f865c0014b592be54d1eda1d022c36e
Paulo Ricardo <139559412+mrpauloricardo@users.noreply.github.com>
2023-11-18 18:09:46 -0300

    Modificada a classe de Disciplina

--- Universidade/src/Disciplina.java
+++ Universidade/src/Disciplina.java
@@ -1,4 +1,17 @@
-
 public class Disciplina {
+    private String nome;
+    private String codigo;
 
+    public Disciplina(String nome, String codigo) {
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void iniciarAula() {
+        System.out.println("Iniciando aula da disciplina " + codigo + " - " + nome);
+    }
 }
