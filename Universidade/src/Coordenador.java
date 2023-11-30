65c77e578c89c672b79386b392e4fd18c2f2acda
Roberta <robertagermano@gmail.com>
2023-11-30 14:05:23 -0300

    Deletado os arquivos .class e modificado o arquivo Coordenador

--- Universidade/src/Coordenador.java
+++ Universidade/src/Coordenador.java
@@ -5,7 +5,15 @@ public class Coordenador {
         this.nome = nome;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void comunicarReitor(Reitor reitor, Disciplina disciplina) {
+        System.out.println(nome + " está se comunicando com o reitor " + reitor.getNome() + " sobre a atribuição de um professor a disciplina de " + disciplina.getNome() + ".");
+    }
+
     public void atribuirProfessorDisciplina(Professor professor, Disciplina disciplina) {
-        System.out.println(nome + " atribuiu o professor " + professor.getNome() + " à disciplina " + disciplina.getNome());
+        System.out.println(nome + " atribuiu " + professor.getNome() + " à disciplina " + disciplina.getNome() + ".");
     }
 }
